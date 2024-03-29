dcb38d1a02f44aeee3af25ca226f42b8fbd02e5e
calvinmcelvain <mcelvain1@hotmail.com>
2024-03-29 12:16:37 -0500

    1.0.2 Admin Instructions page fix (P25 deleted)

--- __admin_exp/__init__.py
+++ __admin_exp/__init__.py
@@ -264,4 +264,4 @@ class P31(BaseReadyPage4):
     pass
 
 
-page_sequence = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31]
+page_sequence = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P17, P18, P19, P20, P21, P22, P23, P24, P26, P27, P28, P29, P30, P31]
