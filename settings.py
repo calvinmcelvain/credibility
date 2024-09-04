bfd32eae223d6e23ccaa779b7e954735e6eae75c
calvinmcelvain <mcelvain1@hotmail.com>
2024-09-04 16:16:25 -0500

    Trying to make session configs dynamic

--- settings.py
+++ settings.py
@@ -1,84 +1,92 @@
-import envi_vars 
 from os import environ
 
-# Session settings (time, groups, rounds)
-GROUPING = int(environ.get('GROUPING'))
-STAGE_1_ROUNDS = int(environ.get('STG_1_ROUNDS'))
+# Setting session configs
+try:
+    # Try to import the env_vars file (works for local development)
+    import envi_vars
+    GROUPING = envi_vars.GROUPING
+    STAGE_1_ROUNDS = envi_vars.STAGE_1_ROUNDS
+    DEMO_PARTICIPANTS = envi_vars.DEMO_PARTICIPANTS
+    SKIP = envi_vars.SKIP
+except ImportError:
+    # Fall back to environment variables (for production on Heroku)
+    GROUPING = environ.get('GROUPING')
+    STAGE_1_ROUNDS = environ.get('STAGE_1_ROUNDS')
+    DEMO_PARTICIPANTS = environ.get('DEMO_PARTICIPANTS')
+    SKIP = environ.get('SKIP')
+
+# Times
 DECISION_TIME = 20000   # In milliseconds (a javascript timeout)
 FEEDBACK_TIME = 20  # In seconds
 INSTRUCTIONS_TIME = None
-SKIP = bool(environ.get('SKIP'))
-
-# Demo participants
-participants = int(environ.get('DEMO_PARTICIPANTS'))
 
 SESSION_CONFIGS = [
     dict(
         name='Stage_1_Instructions_Only',
         display_name="Stage 1: Instructions",
         app_sequence=['start', 'stg1_1'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_1_Game_Only',
         display_name='Stage 1: Game',
         app_sequence=['start', 'stg1_2'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_1',
         display_name='Stage 1',
         app_sequence=['start', 'stg1_1', 'stg1_2'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Scenario_1',
         display_name='Stage 2: Scenario 1',
         app_sequence=['start', 'stg2_1', 'stg2_2'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Scenario_2',
         display_name='Stage 2: Scenario 2',
         app_sequence=['start', 'stg2_3', 'stg2_4'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Scenario_3',
         display_name='Stage 2: Scenario 3',
         app_sequence=['start', 'stg2_5', 'stg2_6'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Scenario_4',
         display_name='Stage 2: Scenario 4',
         app_sequence=['start', 'stg2_7', 'stg2_8'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Instructions_Only',
         display_name='Stage 2: Instructions',
         app_sequence=['start', 'stg2_1', 'stg2_3', 'stg2_5', 'stg2_7'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2_Games',
         display_name='Stage 2: All Scenarios',
         app_sequence=['start', 'stg2_2', 'stg2_4',
                       'stg2_6', 'stg2_8'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_2',
         display_name='Stage 2',
         app_sequence=['start', 'stg2_1', 'stg2_2', 'stg2_3', 'stg2_4', 'stg2_5', 'stg2_6', 'stg2_7', 'stg2_8'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='Stage_1_Stage_2',
         display_name='Full Experiment',
         app_sequence=['start', 'stg1_1', 'stg1_2', 'stg2_1', 'stg2_2', 'stg2_3', 'stg2_4', 'stg2_5', 'stg2_6', 'stg2_7', 'stg2_8'],
-        num_demo_participants=participants,
+        num_demo_participants=DEMO_PARTICIPANTS,
     ),
     dict(
         name='experimenter_page',
