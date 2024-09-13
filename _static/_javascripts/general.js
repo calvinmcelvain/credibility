cee766f407dee1df5284d1273ca99e61b26126b6
calvinmcelvain <mcelvain1@hotmail.com>
2024-09-13 13:20:50 -0500

    Making Stage 1 quiz into 2 pages
    Because there is room, changed the margins

--- _static/_javascripts/general.js
+++ _static/_javascripts/general.js
@@ -53,7 +53,7 @@ function idconfirmation(event) {
 }
 
 /// Stage 1 instructions quiz answer validation
-function CheckQuizAnswers() {
+function CheckQuizAnswersP1() {
     var answers = {
       Q1a: document.querySelector('input[name="Q1a"]').value.trim(),
       Q1b: document.querySelector('input[name="Q1b"]').value.trim(),
@@ -61,10 +61,6 @@ function CheckQuizAnswers() {
       Q2a: document.querySelector('input[name="Q2a"]').value.trim(),
       Q2b: document.querySelector('input[name="Q2b"]').value.trim(),
       Q2c: document.querySelector('input[name="Q2c"]').value.trim(),
-      Q3a: document.querySelector('select[name="Q3a"]').value.trim(),
-      Q3b: document.querySelector('select[name="Q3b"]').value.trim(),
-      Q3c: document.querySelector('select[name="Q3c"]').value.trim(),
-      Q4: document.querySelector('select[name="Q4"]').value.trim(),
     };
 
     var correctAnswers = {
@@ -74,6 +70,39 @@ function CheckQuizAnswers() {
       Q2a: '24',
       Q2b: '12',
       Q2c: '0',
+    };
+
+    var correct = true;
+
+    // Clear previous error messages
+    var errorSpans = document.querySelectorAll('[id^="errorQ"]');
+    errorSpans.forEach(function (errorSpan) {
+      errorSpan.textContent = '';
+      errorSpan.style.display = 'none';
+    });
+
+    // Check if all answers are correct
+    for (var key in answers) {
+      if (answers[key] !== correctAnswers[key]) {
+        showErrorMessage(key);
+        correct = false;
+      }
+    }
+
+    if (correct) {
+      // Submit the form
+      document.forms[0].submit();
+    }
+}
+function CheckQuizAnswersP2() {
+    var answers = {
+      Q3a: document.querySelector('select[name="Q3a"]').value.trim(),
+      Q3b: document.querySelector('select[name="Q3b"]').value.trim(),
+      Q3c: document.querySelector('select[name="Q3c"]').value.trim(),
+      Q4: document.querySelector('select[name="Q4"]').value.trim(),
+    };
+
+    var correctAnswers = {
       Q3a: 'True',
       Q3b: 'False',
       Q3c: 'False',
--- stg1_1/__init__.py
+++ stg1_1/__init__.py
@@ -217,4 +217,8 @@ class P16(BaseReadyPage):
     pass
 
 
-page_sequence = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P10WaitPage, P11, P12, P12WaitPage, P13, P14, P15, P16]
+class P17(BaseReadyPage):
+    pass
+
+
+page_sequence = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P10WaitPage, P11, P12, P12WaitPage, P13, P14, P15, P16, P17]
